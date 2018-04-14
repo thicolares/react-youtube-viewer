9ece7a9a1ded8af2816c30af735b427c49ca4817
Thiago Colares <thicolares@gmail.com>
2018-04-14 17:56:56 -0300

    Create the SearchBar component and import it

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return <input />
+};
+
+// any code that imports search_bar, will have access only to SearchBar
+export default SearchBar;
