74c5fe36e3047f113353ce6138a38b5add58d885
Thiago Colares <thicolares@gmail.com>
2018-04-14 19:02:00 -0300

    Use arrow function for the event handler

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input onChange={this.onInputChange} />;
+        return <input onChange={event => console.log(event.target.value) } />;
     }
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    // Usage: this.onInputChange
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
 }
 
 // any code that imports search-bar, will have access only to SearchBar
