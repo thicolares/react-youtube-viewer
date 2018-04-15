903a88108b38ea9fcee6fce7eb922f6d23d8076b
Thiago Colares <thicolares@gmail.com>
2018-04-15 10:01:14 -0300

    Mock video listing with map

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import VideoListItem from './video-list-item';
 
 const VideoList = (props) => {
     return (
         <ul className="col-nd-4 list-group">
-            <li>{props.videos.length}</li>
+            {props.videos.map( video => <div>1</div>)}
         </ul>
     );
 }
