ce71bdfb77a915cd7b1facb963681021f8d5b5f1
Thiago Colares <thicolares@gmail.com>
2018-04-15 11:31:55 -0300

    Use state's selected video in video detail

--- src/index.js
+++ src/index.js
@@ -23,10 +23,16 @@ class App extends Component {
     constructor(props) {
         super(props)
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -34,7 +40,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos} />
             </div>
         )
