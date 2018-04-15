684e45a900d3dc7784cf71d5af3accc2ccfcb864
Thiago Colares <thicolares@gmail.com>
2018-04-14 21:07:23 -0300

    Create a controlled component

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render() {
-        return <input onChange={event => console.log(event.target.value) } />;
+    // All js classes has the constructor function
+    // reserved for doing some setup
+    constructor(props) {
+        // Call the constructor defined on the parent
+        super(props);
+
+        // This is the ONLY PLACE WHERE we set state like this: this.state.term = 'bli'. Always use this.setState()!!
+        // but you can reference using this.state.term
+        this.state = { term: '' }
     }
 
-    // Usage: this.onInputChange
-    // onInputChange(event) {
-    //     console.log(event.target.value);
-    // }
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value }) }
+                />
+                Value of the input: {this.state.term}
+            </div>
+        );
+    }
+
 }
 
 // any code that imports search-bar, will have access only to SearchBar
