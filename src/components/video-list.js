57f385337b11f87c52fdf1a54fe29f7d88cf9f1e
Thiago Colares <thicolares@gmail.com>
2018-04-15 10:22:15 -0300

    Use unique key prop in VideoList

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -2,9 +2,14 @@ import React from 'react';
 import VideoListItem from './video-list-item';
 
 const VideoList = (props) => {
+    const videoItems =  props.videos.map(video => {
+            return <VideoListItem key={video.etag} video={video}/>
+        }
+    )
+
     return (
         <ul className="col-nd-4 list-group">
-            {props.videos.map( video => <div>1</div>)}
+            {videoItems}
         </ul>
     );
 }
