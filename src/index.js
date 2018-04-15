9411c3da88fb5046463fd3810f1f0d12e3656293
Thiago Colares <thicolares@gmail.com>
2018-04-15 15:35:56 -0300

    Debounce video search call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 // go find the library react and assign it to the variable React
 /// Used to create and manage components
@@ -41,9 +42,13 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce(term => {
+           this.videoSearch(term)
+        }, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
